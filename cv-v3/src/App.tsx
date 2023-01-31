0dfc1373d7338659f34ce93209f104c1b9e541d0
Nikita K <w.feelingalright.p@yandex.ru>
2023-01-31 10:25:00 +0300

    refactor: clear loading, clear app and save new background color

--- cv-v3/src/App.tsx
+++ cv-v3/src/App.tsx
@@ -1,21 +1,5 @@
-import { useState } from 'react';
-
 import Layout from 'page/Layout';
-import Loading from 'components/Loading';
 
-const App = () => {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  const handleLoading = () => setIsLoaded(true);
-
-  window.addEventListener('load', handleLoading);
-
-  return (
-    <>
-      <Loading isLoaded={isLoaded} />
-      <Layout />
-    </>
-  );
-};
+const App = () => <Layout />;
 
 export default App;
--- cv-v3/src/components/Loading/index.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingProps } from './types';
-
-import './index.scss';
-
-const Loading = ({ isLoaded }: LoadingProps) => (
-  <div className={isLoaded ? 'loading-overlay' : 'loading-overlay active'}></div>
-);
-
-export default Loading;
